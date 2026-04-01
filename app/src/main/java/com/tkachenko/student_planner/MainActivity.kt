952ba7b5795c53f4dd3a8559651d616357001c58
karasidnez <karasdnez@gmail.com>
2026-04-01 14:48:30 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/tkachenko/student_planner/MainActivity.kt
+++ app/src/main/java/com/tkachenko/student_planner/MainActivity.kt
@@ -11,6 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.tkachenko.student_planner.navigation.StudentPlannerNavHost
+import com.tkachenko.student_planner.ui.theme.Student_PlannerTheme
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.navigation.compose.rememberNavController
+import com.tkachenko.student_planner.navigation.StudentPlannerNavHost
 import com.tkachenko.student_planner.ui.theme.Student_PlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Student_PlannerTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudentPlannerNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Student_PlannerTheme {
-        Greeting("Android")
     }
 }
